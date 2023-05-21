0579e5723c258f0881ec34e502e1b57aad9d53f8
david.pino <david@ip-172-31-89-46.ec2.internal>
2023-05-21 21:08:17 +0000

    version 0.1.8

---
+++ src/components/CustomColorInput .tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useField } from "formik";
+import styles from '../styles/styles.module.css'
+
+interface Props {
+  name: string;
+  label: string;
+  placeholder?: string;
+  [x: string]: any;
+}
+export const CustomColorInput = ({ label, ...props }: Props) => {
+  const [field] = useField(props);
+
+  return (
+    <div>
+      <label className={styles.field__label}>{label}</label>
+      <input  className={styles.color} {...field} {...props} type="color" />
+    </div>
+  );
+};
